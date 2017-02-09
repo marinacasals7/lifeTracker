1daa15ec92f7438801c0c7287663e0de638f8388
marinacasals7 <marina.casals7@gmail.com>
2017-02-09 10:03:34 +0100

    He canviado para que nos salgan los dias que lo hemos conseguido

--- public/javascripts/goalController.js
+++ public/javascripts/goalController.js
@@ -1,6 +1,12 @@
 angular.module('goalController', [])
 .controller('goalCtrl', function($scope, $http, Goals) {
   Goals.get().success(function(data) {
+    for (var currentIndex=0; currentIndex.length; currentIndex=currentIndex+1) {
+var currentElement= data[currentIndex];
+      currentElement.ok = currentIndex;
+      currentElement.ko =1;
+      currentElement.percentage= (currentElelment.ok/(currentElement.ok+currentElement.ko))*100;
+    }
     $scope.goals = data;
   });
   $scope.addGoal = function() {
